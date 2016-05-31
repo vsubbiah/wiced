ffb0d46ad2c765c62a5a3cf88f91325f6a49edfa
Venkat Subbiah <venkat.subbiah@ubnt.com>
2016-05-31 16:03:54 -0700

    Remove duplicate active scan entries

--- scripts/big_query.py
+++ scripts/big_query.py
@@ -16,18 +16,26 @@ def get_href(node):
 	return node["self"]["href"]
 
 def print_active_scan_data(json_data):
+	duplicate_cnt=0;
 	data = json.loads(json_data)
-	#pprint(data)
-
+	pprint(data)
+	prev_href=""
 	nodes_t = data["nodes"]
 	nodes = sorted(nodes_t, key=get_href)
 	printf ("%50s %20s %10s %5s %20s \n", "href", "bdaddr", "bdaddrType", "rssi", "(ADType, ADValue)")
 	for n in nodes:
-		printf ("%50s %20s %10s %5s", n["self"]["href"], n["bdaddr"], n["bdaddrType"], n["rssi"])
-		ad = n["AD"]
-		for a in ad:
-			printf(" (%s %s) ", a["ADType"], a["ADValue"])
-		printf("\n")
+		href = get_href(n)
+		if href ==  prev_href:
+			duplicate_cnt = duplicate_cnt + 1;
+		else:
+			printf ("%50s %20s %10s %5s", n["self"]["href"], n["bdaddr"], n["bdaddrType"], n["rssi"])
+			duplicate_cnt = 0
+			ad = n["AD"]
+			for a in ad:
+				printf(" (%s %s) ", a["ADType"], a["ADValue"])
+			printf("\n")
+		prev_href = href
+		
 
 def wiced_get_active_scan_data(ipaddr):
 	url='http://%s/gap/nodes?active=1' % (ipaddr)
